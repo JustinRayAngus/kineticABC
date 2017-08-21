30afbb97517a3df010e49c97fe0f77a8a157939f
Angus <angus1@caligula.llnl.gov>
2017-08-21 10:32:39 -0700

    minor changes... moved directory...

---
+++ thisbuild/CMakeFiles/tdb.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/angus1/Programs/TITTS/kineticABC/src/jsoncpp.cpp" "/Users/angus1/Programs/TITTS/kineticABC/thisbuild/CMakeFiles/tdb.dir/jsoncpp.cpp.o"
+  "/Users/angus1/Programs/TITTS/kineticABC/src/main.cpp" "/Users/angus1/Programs/TITTS/kineticABC/thisbuild/CMakeFiles/tdb.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/angus1/HDF5/hdf5-1.10.0-patch1/hdf5/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
